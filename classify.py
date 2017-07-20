7e9ac110cc5d35ebe5855b1ecf8820dc9a2fcdd8
Khayyam Saleem <khayyam.saleem@gmail.com>
2017-07-20 11:47:28 -0600

    testing har_cnn:

---
+++ classify.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+
+plt.style.use('ggplot')
+
+def read_data(file_path):
+    column_names = ['user-id', 'activity', 'timestamp', 'x-axis', 'y-axis', 'z-axis']
+    data = pd.read_csv(file_path, header=None, names=column_names)
+    return data
+
+def feature_normalize(dataset):
+    mu = np.mean(dataset, axis=0)
+    sigma = np.std(dataset, axis=0)
+    return (dataset - mu)/sigma
+
+def plot_axis(ax, x, y, title):
+    ax.plot(x,y)
+    ax.set_title(title)
+    ax.xaxis.set_visible(False)
+    ax.set_ylim([min(y) - np.std(y), max(y) + np.std(y)])
+    ax.set_xlim([min(x), max(x)])
+    ax.grid(True)
+
+def plot_activity(activity, data):
+    fig, (ax0, ax1, ax2) = plt.subplots(nrows=3, figsize = (15, 10), sharex = True)
+    plot_axis(ax0, data['timestamp'], data['x-axis'], 'x-axis')
+    plot_axis(ax1, data['timestamp'], data['y-axis'], 'y-axis')
+    plot_axis(ax2, data['timestamp'], data['z-axis'], 'z-axis')
+    plt.subplots_adjust(hspace=0.2)
+    fig.suptitle(activity)
+    plt.subplots_adjust(top=0.90)
+    plt.show()
+
+dataset = read_data('actitracker_raw.txt')
+print("good")
+dataset['x-axis'] = feature_normalize(dataset['x-axis'])
+print("good")
+dataset['y-axis'] = feature_normalize(dataset['y-axis'])
+print("good")
+dataset['z-axis'] = feature_normalize(dataset['z-axis'])
+print("good")
+
+for activity in np.unique(dataset["activity"]):
+    print("working")
+    subset = dataset[dataset["activity"] == activity][:180]
+    plot_activity(activity, subset)
+
+
+
